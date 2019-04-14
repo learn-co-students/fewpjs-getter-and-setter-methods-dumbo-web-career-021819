9019bb5d54d12f08aea4cc912f7a794a97b23ee4
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-04-14 16:37:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your Circle class here
+
+class Circle {
+
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  // Getter Methods
+
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  get area() {
+    return (this.radius ** 2) * Math.PI;
+  }
+
+  get circumference() {
+    return this.diameter * Math.PI;
+  }
+
+  // Setter Methods
+
+  set diameter(num) {
+    this.radius = num / 2;
+    return num;
+  }
+
+  set area(num) {
+    this.radius = Math.sqrt(num) / Math.PI;
+    return num;
+  }
+
+  set circumference(num) {
+    this.radius = num / Math.PI / 2;
+    return num;
+  }
+
+}
